00f9afe560a534ad49624bbd55c02b9c978e27f0
Myah <myah@Zipcoders-MacBook-Pro-61.local>
2022-07-26 07:01:27 -0400

    attempted pig latin generator

--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/PigLatinGenerator.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/PigLatinGenerator.java
@@ -4,6 +4,11 @@ package rocks.zipcode.io.quiz3.fundamentals;
  * @author leon on 09/12/2018.
  */
 public class PigLatinGenerator {
+    static boolean isVowel(char ch) {
+        return (ch == 'A' || ch == 'a' || ch == 'E' || ch == 'e' || ch == 'I' ||
+                ch == 'i' || ch == 'O' || ch == 'o' || ch == 'U' || ch == 'u');
+    }
+
     public String translate(String str) {
         return null;
     }
