9fa8140d6d6ebc3a04ada895168015800d953a1d
Myah <myah@Zipcoders-MacBook-Pro-61.local>
2022-07-26 06:43:06 -0400

    few vowel test cases pass

--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/VowelUtils.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/VowelUtils.java
@@ -1,5 +1,9 @@
 package rocks.zipcode.io.quiz3.fundamentals;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author leon on 09/12/2018.
  */
@@ -9,15 +13,39 @@ public class VowelUtils {
     }
 
     public static Integer getIndexOfFirstVowel(String word) {
+//        String[] vowels = new String[] { "A", "a", "E", "e", "I", "i", "O",
+//                "o", "U", "u" };
+//        List<String> vowelList = new ArrayList<String>(Arrays.asList(vowels));
+//        for (int i = 0; i < word.length(); i++) {
+//            if(vowelList.contains(word.charAt(i))){
+//                return i;
+//            }
+//        }
         return null;
     }
 
 
     public static Boolean startsWithVowel(String word) {
-        return null;
+        String[] VOWELS ={"a","e","i","o","u"};
+        for (String vowel : VOWELS) {
+            if (word.toLowerCase().startsWith(vowel.toLowerCase()))
+                return true;
+        }
+        return false;
+//        return null;
     }
 
     public static Boolean isVowel(Character character) {
-        return null;
+        Character charA = 'a';
+        Character charE = 'e';
+        Character charI = 'i';
+        Character charO = 'o';
+        Character charU = 'u';
+        if (character == 'a' || character == 'e' || character == 'i' || character == 'o'
+                || character == 'u' || character == 'A' || character == 'E' || character == 'I'
+                || character == 'O' || character == 'U') {
+            return true;
+        }
+        return false;
     }
 }
